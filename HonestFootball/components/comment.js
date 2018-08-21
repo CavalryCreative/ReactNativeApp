57b0e7886a033c66fce802d00f33cd151dcc48d3
pablodance <chunk_pd@yahoo.co.uk>
2018-08-21 18:03:27 +0100

    colours

--- HonestFootball/components/comment.js
+++ HonestFootball/components/comment.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, ListView  } from 'react-native';
+import { Text, View, StyleSheet, ListView  } from 'react-native';
 
 export default class Comment extends React.Component {
 
@@ -27,20 +27,38 @@ export default class Comment extends React.Component {
               if(homeTeamId === selectedTeamId)
               {
                  return (
-                      <View>
+                 
+                      <View style={{flex: 1}}>
+                       <View style={{flex: 1, backgroundColor: 'powderblue'}} >
                         <Text>{this.props.dataSource[0].LatestEvent.Minute}</Text>
+                        </View>
+                         <View style={{flex: 8, backgroundColor: 'skyblue'}} >
                         <Text>{this.props.dataSource[0].LatestEvent.HomeComment}</Text>
                         <Text>{this.props.dataSource[0].LatestEvent.Score}</Text>
+                        </View>
                       </View>
                     );
               }
               else
               {
                   return (
-                      <View>
-                        <Text>{this.props.dataSource[0].LatestEvent.Minute}</Text>
-                        <Text>{this.props.dataSource[0].LatestEvent.AwayComment}</Text>
-                        <Text>{this.props.dataSource[0].LatestEvent.Score}</Text>
+
+                      <View style={{flex:1, width:380}}>
+
+                       <View style={{flex: 2, backgroundColor: 'white', marginTop:30}} >
+                          <View style={styles.timBox} >
+                          <Text style={styles.timeSml}>Time:</Text>
+                            <Text style={styles.time}>{this.props.dataSource[0].LatestEvent.Minute}</Text>
+                          </View>
+                          <View style={{width:150}} >
+                              <Text style={{color: 'white'}}>{this.props.dataSource[0].LatestEvent.Score}</Text>
+                                       </View>
+
+ </View>
+                         <View style={{flex: 10, backgroundColor: '#b4281e', color: 'white'}} >
+                        <Text style={styles.lrgComment}>{this.props.dataSource[0].LatestEvent.AwayComment}</Text>
+                      
+                        </View>
                       </View>
                     );
               }
@@ -56,3 +74,87 @@ export default class Comment extends React.Component {
       }
   }
 }
+
+const styles = StyleSheet.create({
+  lrgComment: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 50,
+    textAlignVertical: "bottom",
+    textAlign: "left",
+  flexDirection: "row",
+    fontFamily: 'Helvetica-Bold',
+    padding: 15,
+    textAlignVertical: "bottom",
+    position: 'absolute',
+    bottom:0,
+
+
+  },
+  //    timeBlock: {
+  //  border:'1px'
+   
+
+
+  // },
+   time: {
+    color: '#b4281e',
+    fontWeight: 'bold',
+    fontSize: 50,
+  
+    textAlign: "left",
+  
+    fontFamily: 'Helvetica-Bold',
+   paddingRight:15,
+   paddingLeft:15,
+   paddingTop:-5,
+   paddingBottom:0,
+
+  
+   
+
+
+  },
+   timBox: {
+    width:170,
+    borderRightWidth:1,
+    borderColor:'#b4281e',
+    marginBottom:0,
+   
+    
+   
+
+
+  },
+  timeSml: {
+    color: '#212e51',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginTop:0,
+     padding: 15,
+     paddingTop:0,
+     marginBottom:0,
+     paddingBottom:0,
+    
+   
+
+
+  },
+  scoreLine: {
+    color: '#212e51',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginTop:0,
+     padding: 15,
+     paddingTop:0,
+     marginBottom:0,
+     paddingBottom:0,
+    
+   
+
+
+  },
+  red: {
+    color: 'red',
+  },
+});
---
+++ HonestFootball/styles/styles.js
@@ -0,0 +1,26 @@
+var React = require('react-native');
+
+var {
+  StyleSheet,
+} = React;
+
+module.exports = StyleSheet.create({
+  lrgComment: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 40,
+    textAlignVertical: "bottom",
+    textAlign: "left",
+  flexDirection: "row",
+    fontFamily: 'Helvetica-Bold',
+    padding: 15,
+    textAlignVertical: "bottom",
+    position: 'absolute',
+    bottom:0,
+
+
+  },
+  red: {
+    color: 'red',
+  },
+});
